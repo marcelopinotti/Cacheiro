f76535bd8ffcd258dd5fe1d6347ca42b2bc5b538
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-07-17 12:28:20 -0300

    feat: update product service methods for improved retrieval by ID

--- catalogo-service/src/main/java/com/dev/cacheiro/catalogo/service/ProdutoService.java
+++ catalogo-service/src/main/java/com/dev/cacheiro/catalogo/service/ProdutoService.java
@@ -4,8 +4,8 @@ package com.dev.cacheiro.catalogo.service;
 import com.dev.cacheiro.catalogo.dtos.ProdutoMapper;
 import com.dev.cacheiro.catalogo.dtos.ProdutoRequest;
 import com.dev.cacheiro.catalogo.dtos.ProdutoResponse;
+import com.dev.cacheiro.catalogo.entity.Produto;
 import com.dev.cacheiro.catalogo.repository.ProdutoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +13,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.list;
 
 @Service
 @RequiredArgsConstructor
@@ -26,16 +29,18 @@ public class ProdutoService {
     private final ProdutoMapper mapper;
 
     @Transactional(readOnly = true)
-    public List<ProdutoResponse> listarPorId() {
+    public ProdutoResponse buscarPorId(Long id) {
         simularBanco();
-        return repository.findAll().stream().map(mapper::toResponse).toList();
+        return mapper.toResponse(findProdutoById(id));
     }
 
     @Transactional(readOnly = true)
-    public ProdutoResponse listar(Long id){
+    public List<ProdutoResponse> listar(){
         simularBanco();
-        return repository.findById(id).map(mapper::toResponse)
-                .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+        return repository.findAllByOrderByIdAsc()
+                .stream()
+                .map(mapper::toResponse)
+                .toList();
     }
 
     @Transactional
@@ -48,8 +53,7 @@ public class ProdutoService {
 
     @Transactional
     public ProdutoResponse atualizar(Long id, ProdutoRequest request) {
-        var produtoExiste = repository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Produto não encontrado"));
+        var produtoExiste = findProdutoById(id);
         produtoExiste.setNome(request.nome());
         produtoExiste.setDescricao(request.descricao());
         produtoExiste.setPreco(request.preco());
@@ -62,6 +66,11 @@ public class ProdutoService {
         repository.deleteById(id);
     }
 
+    private Produto findProdutoById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+
     private void simularBanco() {
         try{
             Thread.sleep(latenciaMs);
@@ -72,4 +81,3 @@ public class ProdutoService {
 
 
 }
-
