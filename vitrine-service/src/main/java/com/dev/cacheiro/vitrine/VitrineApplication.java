713d5b90e7cec7930588aa667b1c84cf1d3705bb
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-07-17 14:36:44 -0300

    feat: enable configuration properties scanning in VitrineApplication

--- vitrine-service/src/main/java/com/dev/cacheiro/vitrine/VitrineApplication.java
+++ vitrine-service/src/main/java/com/dev/cacheiro/vitrine/VitrineApplication.java
@@ -2,8 +2,10 @@ package com.dev.cacheiro.vitrine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class VitrineApplication {
 
 	public static void main(String[] args) {
