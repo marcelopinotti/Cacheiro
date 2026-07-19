6230a2a3724bb418abf2965cdebc1adaff984bd4
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-07-19 12:26:53 -0300

    refactor: remove unused AllArgsConstructor annotation from Produto class

--- catalogo-service/src/main/java/com/dev/cacheiro/catalogo/entity/Produto.java
+++ catalogo-service/src/main/java/com/dev/cacheiro/catalogo/entity/Produto.java
@@ -1,7 +1,6 @@
 package com.dev.cacheiro.catalogo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,7 +11,6 @@ import java.math.BigDecimal;
 @Table(name = "produtos")
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Produto {
 
